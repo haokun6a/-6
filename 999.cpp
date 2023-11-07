5469667bafd9abb36e4e0ce1d0830734c0351f53
haokun6a <149570651+haokun6a@users.noreply.github.com>
2023-11-07 21:08:26 +0800

    Add files via upload

---
+++ 999.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	float a[3][4];
+	int i,j,maxindex1,maxindex2;
+	
+	for(i=0;i<=2;i++){
+		for(j=0;j<=3;j++)
+	{
+		scanf("%f",&a[i][j]);
+		}	
+	}
+	
+	for(i=0;i<=2;i++)
+	{
+		for(j=0;j<=3;j++)
+		{
+			if(a[i][j]>a[i][j+1])
+			{
+				maxindex1=i;
+				maxindex2=j;
+			}
+		}
+	}
+	
+	printf("%.2f",a[maxindex1][maxindex2]);
+	return 0;
+}
